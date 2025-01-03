5e133d9db9d22f552a22b12502ae9aeb9c31f757
Arthur <arthur.fernando03@gmail.com>
2025-01-02 21:36:26 -0300

    generate authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  validates :name, presence: true
-  validates :birthdate, presence: true
-  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  has_secure_password
+  has_many :sessions, dependent: :destroy
+
+  normalizes :email_address, with: ->(e) { e.strip.downcase }
 end
